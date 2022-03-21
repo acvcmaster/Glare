94f3def64912b309708403f10608f74a1b4e6fc6
acvcmaster <acvcnos@hotmail.com>
2022-03-21 01:36:31 -0300

    Refactoring tests

--- src/tests/literal.rs
+++ src/tests/literal.rs
@@ -1,28 +1,40 @@
-#[cfg(test)]
-mod literal_tests {
-    use crate::{parser::Parser, tokenizer::Tokenizer};
+use crate::{
+    parser::{Literal, Parser},
+    tokenizer::Tokenizer,
+};
 
-    #[test]
-    fn string_literal_ok() {
-        let mut tokenizer = Tokenizer::new("`Literal string`");
-        let mut parser = Parser::new(&mut tokenizer);
+#[test]
+fn string_literal_ok() {
+    let mut tokenizer = Tokenizer::new("`Literal string`");
+    let mut parser = Parser::new(&mut tokenizer);
 
-        assert!(parser.parse_literal(true).is_ok())
-    }
+    assert!(match parser.parse_literal(false) {
+        Ok(value) => match value {
+            Literal::StringLiteral(value) => value == "Literal string",
+            Literal::NumberLiteral(_) => false,
+        },
+        _ => false,
+    })
+}
 
-    #[test]
-    fn number_literal_ok() {
-        let mut tokenizer = Tokenizer::new("42");
-        let mut parser = Parser::new(&mut tokenizer);
+#[test]
+fn number_literal_ok() {
+    let mut tokenizer = Tokenizer::new("42");
+    let mut parser = Parser::new(&mut tokenizer);
 
-        assert!(parser.parse_literal(true).is_ok())
-    }
+    assert!(match parser.parse_literal(false) {
+        Ok(value) => match value {
+            Literal::StringLiteral(_) => false,
+            Literal::NumberLiteral(value) => value == 42,
+        },
+        _ => false,
+    })
+}
 
-    #[test]
-    fn literal_err() {
-        let mut tokenizer = Tokenizer::new("# Comment");
-        let mut parser = Parser::new(&mut tokenizer);
+#[test]
+fn literal_err() {
+    let mut tokenizer = Tokenizer::new("# Comment");
+    let mut parser = Parser::new(&mut tokenizer);
 
-        assert!(parser.parse_literal(true).is_err())
-    }
+    assert!(parser.parse_literal(true).is_err())
 }
